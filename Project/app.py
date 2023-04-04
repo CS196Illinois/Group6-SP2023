32257b4c0bae9f2f8450a9aa645973f9f77688e0
Ritul Soni <rsoni27@illinois.edu>
2023-04-04 17:04:15 -0500

    added basic frontend template

--- Project/app.py
+++ Project/app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session
+import spotipy
+import spotipy.util as util
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome to Sortify!"
+
+app.secret_key = 'supersecretkey'
+CLIENT_ID = 'your_client_id'
+CLIENT_SECRET = 'your_client_secret'
+REDIRECT_URI = 'http://localhost:5000/callback'
+
+@app.route('/')
+def index():
+    if 'access_token' in session:
+        sp = spotipy.Spotify(auth=session.get('access_token'))
+        user = sp.current_user()
+        return f"Welcome {user['display_name']}!"
+    else:
+        return render_template('index.html', client_id=CLIENT_ID, redirect_uri=REDIRECT_URI)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    token = util.oauth2.SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope='user-read-private user-read-email', cache_path='.cache').get_access_token(code)
+    session['access_token'] = token['access_token']
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
