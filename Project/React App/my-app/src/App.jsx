5728405f31875b2fd0ad49c83a72ad6c80b85e16
Ritul Soni <rsoni27@illinois.edu>
2023-04-25 17:17:47 -0500

    fixed routing

--- Project/React App/my-app/src/App.jsx
+++ Project/React App/my-app/src/App.jsx
@@ -1,48 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
-//import Home from './Home';
+import Home from './Home';
 
 const AUTH_URL = 'https://accounts.spotify.com/authorize';
-const REDIRECT_URI = 'http://localhost:3000/home';
+const REDIRECT_URI = 'http://localhost:3000/callback';
 const CLIENT_ID = 'dd4fcb9412ea4f6bab19c5bb8a4021c0';
 
-const App = () => {
-  const [code, setCode] = useState('');
-
+const Login = () => {
   const handleLogin = () => {
     window.location = `${AUTH_URL}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`;
-  };
-
-  const handleCallback = async () => {
-    const { data } = await axios.post('https://accounts.spotify.com/api/token', {
-      grant_type: 'authorization_code',
-      code,
-      redirect_uri: REDIRECT_URI,
-      client_id: CLIENT_ID,
-      client_secret: '6b50f4087c3c4d9da1728499e497a540',
-    });
-  
-    // Store the access token in local storage
-    localStorage.setItem('access_token', data.access_token);
-  
-    // Redirect the user to the home page
-    window.location.href = '/home';
-   
   };
 
   return (
     <div className="login-page">
       <h1 className="logo">Spotify</h1>
       <p className="tagline">Get Your Groove On</p>
-      <div className="form">
-        <h2 className="form-title">Log in to your account</h2>
-        <input className="form-input" type="text" value={code} onChange={(e) => setCode(e.target.value)} placeholder="Enter your authorization code" />
-        <button className="form-button" onClick={handleCallback}>Submit code</button>
-      </div>
       <button className="spotify-button" onClick={handleLogin}>Log in with Spotify</button>
     </div>
   );
 };
 
+const Callback = () => {
+  const handleCallback = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+    if (code) {
+      const { data } = await axios.post('https://accounts.spotify.com/api/token', {
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: REDIRECT_URI,
+        client_id: CLIENT_ID,
+        client_secret: '6b50f4087c3c4d9da1728499e497a540',
+      });
+
+      // Store the access token in local storage
+      localStorage.setItem('access_token', data.access_token);
+
+      // Redirect the user to the home page
+      window.location.href = '/home';
+    }
+  };
+
+  useEffect(() => {
+    handleCallback();
+  }, []);
+
+  return <div>Loading...</div>;
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
 export default App;
+
+
--- Project/React App/my-app/src/Home.jsx
+++ Project/React App/my-app/src/Home.jsx
@@ -1,54 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
-
-const AUTH_URL = 'https://accounts.spotify.com/authorize';
-const REDIRECT_URI = 'http://localhost:3000/callback';
-const CLIENT_ID = 'dd4fcb9412ea4f6bab19c5bb8a4021c0';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const [savedSongs, setSavedSongs] = useState([]);
+  const [accessToken, setAccessToken] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const accessToken = localStorage.getItem('access_token');
-    if (accessToken) {
-      axios.get('https://api.spotify.com/v1/me/tracks', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-        .then((response) => {
-          setSavedSongs(response.data.items);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      setAccessToken(token);
     } else {
-      window.location = `${AUTH_URL}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`;
+      navigate('/');
     }
-  }, []);
-
-  const handleLogout = () => {
-    localStorage.removeItem('access_token');
-    window.location.reload();
-  };
+  }, [navigate]);
 
   return (
     <div className="home-page">
-      <h1 className="logo">Spotify</h1>
-      <button className="logout-button" onClick={handleLogout}>Log out</button>
-      <h2 className="page-title">Your Saved Songs</h2>
-      <ul className="song-list">
-        {savedSongs.map((song) => (
-          <li className="song" key={song.track.id}>
-            <img className="song-image" src={song.track.album.images[0].url} alt={song.track.name} />
-            <div className="song-details">
-              <h3 className="song-name">{song.track.name}</h3>
-              <p className="song-artist">{song.track.artists.map((artist) => artist.name).join(', ')}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <h1 className="welcome">Welcome to Spotify</h1>
+      <p className="access-token">Your access token is: {accessToken}</p>
     </div>
   );
 };
